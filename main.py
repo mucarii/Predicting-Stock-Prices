f37400ad7eef06545ddc7b901e0925abdf15e62e
Murilo <murilo.l.calore.r@gmail.com>
2024-10-29 18:51:27 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# %% [bibliotecas]
+import csv
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+# %%
+dates = []
+close_price = []
+
+
+# %%
+def get_data(GME_stock):
+    global close_price  # Adicionando global para modificar a variável
+    with open(GME_stock, "r") as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)  # Pular o cabeçalho
+        for row in csvFileReader:
+            dates.append(int(row[0].split("-")[0]))  # Ano
+            close_price.append(float(row[4]))  # Preço de fechamento
+
+
+# %%
+def predict_prices(dates, prices, x):
+    dates = np.reshape(dates, (len(dates), 1))  # Convertendo em uma coluna
+
+    svr_lin = SVR(kernel="linear", C=1e3)
+    svr_poly = SVR(kernel="poly", C=1e3, degree=2)
+    svr_rbf = SVR(kernel="rbf", C=1e3, gamma=0.1)  # gamma = 0.1
+    svr_lin.fit(dates, prices)
+    svr_poly.fit(dates, prices)
+    svr_rbf.fit(dates, prices)
+
+    plt.scatter(dates, prices, color="black", label="Dados")
+    plt.plot(dates, svr_lin.predict(dates), color="green", label="Linear")
+    plt.plot(dates, svr_poly.predict(dates), color="blue", label="Polinomial")
+    plt.plot(dates, svr_rbf.predict(dates), color="red", label="RBF")
+    plt.xlabel("Data")
+    plt.ylabel("Preço")
+    plt.legend()
+    plt.show()
+
+    return (
+        svr_lin.predict(np.array([[x]]))[0],
+        svr_poly.predict(np.array([[x]]))[0],
+        svr_rbf.predict(np.array([[x]]))[0],
+    )
+
+
+# %%
+get_data("GME_stock.csv")
+predictions = predict_prices(dates, close_price, 29)
+
+# %%
+print(predictions)
